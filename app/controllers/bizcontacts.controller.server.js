686efff567f0459939f29f3a3feca0202cebb814
Josh <altord@protonmail.com>
2022-10-22 20:29:11 -0400

    Attempt at editing
    Got everything but the editing part done for whatever reason. Refuses to post.

--- app/controllers/bizcontacts.controller.server.js
+++ app/controllers/bizcontacts.controller.server.js
@@ -7,6 +7,60 @@ export function DisplayBizContactsList(req, res, next){
             res.end(err);
         }
 
-        res.render('index', {title: 'Business Contacts List', page: 'bizcontacts/list', bizContacts: bizcontactsCollection})
+        res.render('index', {title: 'Business Contacts List', page: 'bizcontacts/list', bizcontacts: bizcontactsCollection})
     })
 }
+
+///
+
+///
+
+export function DisplayBizContactsEditPage(req, res, next){
+    let id = req.params.id;
+
+    bizcontactsModel.findById(id, (err, bizcontact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {title: 'Edit Business Contact', page: 'bizcontacts/edit', bizcontacts: bizcontact });
+    });    
+}
+
+export function ProcessBizContactsEditPage(req, res, next){
+
+    let id = req.params.id;
+
+    let newContact = bizcontactsModel({
+        _id: req.body.id,
+        contactName: req.body.contactName,
+        contactNumber: req.body.contactNumber,
+        emailAddress: req.body.emailAddress,
+    });
+
+    //REFUSE TO UPDATE I HAVE NO CLUE WHY
+    bizcontactsModel.updateOne({_id: id }, newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/bizcontacts-list')
+    } )
+}
+
+
+export function ProcessBizContactsDelete(req, res, next){
+    let id = req.params.id;
+
+    bizcontactsModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/bizcontacts-list');
+    })
+}
+
--- app/routes/bizcontacts.route.server.js
+++ app/routes/bizcontacts.route.server.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
-import { DisplayBizContactsList } from "../controllers/bizcontacts.controller.server.js";
+import { DisplayBizContactsList,
+    DisplayBizContactsEditPage,
+    ProcessBizContactsEditPage,
+    ProcessBizContactsDelete } from "../controllers/bizcontacts.controller.server.js";
 
 const router = Router();
 
+
 router.get('/bizcontacts-list', DisplayBizContactsList);
+router.post('/bizcontacts-edit/:id', ProcessBizContactsEditPage);
+router.get('/bizcontacts-edit/:id', DisplayBizContactsEditPage);
+router.get('/bizcontacts-delete/:id', ProcessBizContactsDelete);
 
 export default router;
